ced931bb1e4cdd9a5e3aa8a134e97bd0b1346c45
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-10-18 05:23:54 +0300

    .

--- bot.py
+++ bot.py
@@ -75,7 +75,8 @@ def main():
         svg_data_dir = Path("I:/SVG/svg_data")
 
     for title in titles:
-        output_dir = svg_data_dir / title.split("/")[1]
+        # output_dir = svg_data_dir / title.split("/")[1]
+        output_dir = svg_data_dir / Path(title).name
         one_title(title, output_dir, titles_limit=1000)
         # break
 
