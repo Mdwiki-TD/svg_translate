acb079342f264f1b87e16a9fcdf38f522bbe563f
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-10-20 04:39:53 +0300

    Remove download_commons_svgs and refactor downloads
    Deleted the unused download_commons_svgs function and its references from svg_translate. Updated download logic in web modules to use download_one_file directly, improving clarity and reducing redundancy.

--- src/svg_translate/commons/__init__.py
+++ src/svg_translate/commons/__init__.py
@@ -2,12 +2,10 @@
 
 from .upload_bot import upload_file
 from .text_bot import get_wikitext
-from .download_bot import download_commons_svgs
 from .temps_bot import get_files
 
 __all__ = [
     "upload_file",
     "get_wikitext",
-    "download_commons_svgs",
     "get_files",
 ]
