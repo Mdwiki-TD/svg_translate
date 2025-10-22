73c257ad4b437416a2703bf8064da491cd3eaade
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-10-22 07:07:08 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,18 @@
 import sys
+import requests
+import tqdm
+import mwclient
 import types
+import wikitextparser
+import cryptography
+import flask_limiter
+import mwoauth
 from pathlib import Path
-
 
 ROOT = Path(__file__).resolve().parents[1]
 
 if str(ROOT) not in sys.path:
+    sys.path.insert(0, str(ROOT/"src"))
     sys.path.insert(0, str(ROOT))
+
+from src import svg_config  # load_dotenv()
