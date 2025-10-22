966c8436e5074fc531b6c6f47fc59e952a16399c
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-10-22 04:42:03 +0300

    .

--- src/web/upload_task.py
+++ src/web/upload_task.py
@@ -228,9 +228,9 @@ def upload_task(
 
     stages["message"] = (
         f"Total Files: {total:,}, "
-        f"uploaded {upload_result['done']:,}, "
-        f"no changes: {upload_result['no_changes']:,}, "
-        f"not uploaded: {upload_result['not_done']:,}"
+        f"uploaded {upload_result.get('done', 0):,}, "
+        f"no changes: {upload_result.get('no_changes', 0):,}, "
+        f"not uploaded: {upload_result.get('not_done', 0):,}"
     )
 
     if upload_result["not_done"]:
